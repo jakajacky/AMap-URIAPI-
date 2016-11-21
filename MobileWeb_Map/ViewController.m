440986a8227fb2ff64b05d1e84a2408482a677e3
xqzh <xqzh@ideabinder.com>
2016-11-21 19:18:25 +0800

    first

--- MobileWeb_Map/ViewController.m
+++ MobileWeb_Map/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
 
+  @property (strong, nonatomic) IBOutlet UIWebView *webView;
+  
+  
 @end
 
 @implementation ViewController
@@ -17,6 +20,9 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  NSURL *url = [NSURL URLWithString:@"http://m.amap.com/?from=39.997361,116.478346(from)&to=39.966577,116.3246(to)"];
+  NSURLRequest *request = [NSURLRequest requestWithURL:url];
+  [_webView loadRequest:request];
 }
 
 
@@ -24,6 +30,28 @@
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
-
+  
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+  
+}
+  
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+  NSLog(@"---%@---", webView.request.URL);
+}
+  
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+  
+}
+  
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+  if ([[[request.URL absoluteString] substringToIndex:23] isEqualToString:@"http://itunes.apple.com"]) {
+    NSURL *url = [NSURL URLWithString:@"http://m.amap.com/?from=39.997361,116.478346(from)&to=39.966577,116.3246(to)"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:request];
+    
+    return NO;
+  }
+  return YES;
+}
 
 @end
